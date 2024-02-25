606c8022d4058b3d3e2ae0a5931929a6eaddbd62
DIY-It-All <138210699+DIY-It-All@users.noreply.github.com>
2024-02-25 16:41:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('hello;)');
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.get('/', (req,res)=>{
+    res.render('home');
+});
+app.listen(3000, ()=>{
+    console.log('running on port 3000');
+});
