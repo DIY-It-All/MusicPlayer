832a04275a5836bee1d191648e921e5969190784
DIY-It-All <138210699+DIY-It-All@users.noreply.github.com>
2024-02-26 12:17:48 +0000

    included file-upload system, plz add the way to download file to server and sending the data of all files to web page

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
+var multer = require('multer');
+var upload = multer({dest: './public/'});
+app.use(upload.single('file'));
 app.get('/', (req,res)=>{
     res.render('home');
 });
+app.post('/add', (req,res)=>{
+    console.log(req.file);
+    console.log(req.files);
+    res.redirect('/')
+})
 app.listen(3000, ()=>{
     console.log('running on port 3000');
 });
