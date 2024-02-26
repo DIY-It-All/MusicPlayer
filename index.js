a571cc6182fa04eea929a378dd9f7624a611cfaf
DIY-It-All <1983.tanu.gupta@gmail.com>
2024-02-26 18:33:33 +0530

    new update, music can be played and added, cant run on codespace as audio files too big for upload

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
-var multer = require('multer');
-var upload = multer({dest: './public/'});
-app.use(upload.single('file'));
+const fileUpload = require('express-fileupload');
+const path = require('path');
+const fs = require('fs');
+const { fileLoader } = require('ejs');
 app.get('/', (req,res)=>{
     res.render('home');
 });
-app.post('/add', (req,res)=>{
-    console.log(req.file);
-    console.log(req.files);
+app.post('/add',fileUpload({createParentPath: true}), (req,res)=>{
+    const files = req.files
+        console.log(files)
+
+        Object.keys(files).forEach(key => {
+            const filepath = path.join(__dirname, 'public', files[key].name)
+            files[key].mv(filepath, (err) => {
+                if (err) return res.status(500).json({ status: "error", message: err })
+            })
+        })
+
     res.redirect('/')
+})
+app.get('/musicList', (req,res)=>{
+    fileList = fs.readdirSync(path.join(__dirname, 'public'));
+    outArr = [];
+    for(file of fileList){
+        outArr.push({
+            name: file.split('.')[0],
+            path: file
+
+        })
+    }
+    res.json({data: outArr});
 })
 app.listen(3000, ()=>{
     console.log('running on port 3000');
